a3d6aadd3b8d22f8385cf102d58d0dde9acd1108
Andre Escobar <andre.ev@outlook.com>
2022-03-29 00:19:33 +0000

    Ok

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,11 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let suma = 0;
+  let newArray = num.split("").reverse();
 
+  newArray.forEach( function(elemnt, index) {
+    suma = suma + (elemnt * Math.pow(2, index));
+  });
+  
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let newArray = [];
+
+  do {
+    newArray.push(num % 2);
+    num = Math.trunc(num / 2);
+  } while(num > 0);
+
+  let newString = newArray.reverse().toString();
+
+  return newString.replace(/,/g,"");
 
 }
 
