8c33d73c8ab755be6a4217764928d4834f4b1927
Andre Escobar <aescobar@LAPTOP-LL1RGKNG>
2022-11-07 16:38:41 -0500

    Exercise 1 and 2 completed

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -7,6 +7,8 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  var count = 0;
+  return () => ++count;
 }
 
 function cacheFunction(cb) {
@@ -21,6 +23,14 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var newObj = {};
+
+  return (arg) => {
+    return newObj.hasOwnProperty(arg) ? newObj[arg] : (
+      newObj[arg] = cb(arg),
+      newObj[arg]
+    )
+  }
 }
 
 // Bind
