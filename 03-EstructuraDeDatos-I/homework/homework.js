8ca6dbd5325901d2c0b090779f38b16c37767e84
André Escobar <aescobar@synco.pe>
2022-11-09 16:16:51 -0500

    03-DataStructure-I finished

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -7,6 +7,8 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+  if(n <= 1) return 1;
+  return n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
@@ -15,8 +17,11 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
+  return n < 2 ? n : nFibonacci(n - 1) + nFibonacci(n - 2)
 }
+
+
+
 
 // Para esta parte no es necesario utilizar recursión.
 // Implementa la clase Queue que debe contener los siguientes métodos:
@@ -25,8 +30,20 @@ function nFibonacci(n) {
 // size: Devuelve el número de elementos que contiene la queue.
 
 function Queue() {
-
+  this.arr = [];
 }
+
+Queue.prototype.enqueue = function (e) {
+  this.arr.push(e);
+};
+
+Queue.prototype.dequeue = function () {
+  return this.arr.shift();
+}
+
+Queue.prototype.size = function () {
+  return this.arr.length;
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
