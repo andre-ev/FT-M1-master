b37c5cc14e2be3b753c7448518d9c7265f921029
Andre Escobar <aescobar@LAPTOP-LL1RGKNG>
2022-11-11 15:00:08 -0500

    demo upload

---
+++ 04-EstructuraDeDatos-II/demo/demo.js
@@ -0,0 +1,44 @@
+function LinkedList() {
+    this.head = null;
+    this.size = 0; 
+}
+
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+LinkedList.prototype.add = function (value) {
+    const newNode = new Node(value);
+    let current = this.head;
+    
+    if (!current) {
+        this.head = newNode;
+        this.size++;
+        return newNode;
+    }
+
+    while (current.next) {
+        current = current.next;
+    }
+    
+    current.next = newNode;
+    this.size++;
+};
+
+
+var newList = new LinkedList();
+newList.add(5);
+console.log(newList);
+newList.add(15);
+console.log(newList);
+newList.add(75);
+console.log(newList);
+newList.add(105);
+console.log(newList);
+newList.add(115);
+console.log(newList);
+newList.add(1115);
+
+
+console.log(newList);
